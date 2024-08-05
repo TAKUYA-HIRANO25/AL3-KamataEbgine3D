e21ab15086ebb52f2d3a7ae3c68a3717caba864a
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2024-08-05 12:48:05 +0900

    02_12_課題完了

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,11 +1,43 @@
 #include "TitleScene.h"
+#include <numbers>
+#define _USE_MATH_DEFINES
+#include<math.h>
 
-void TitleScene::Initialize() {}
+void TitleScene::Initialize() { 
+	dxCommon_ = DirectXCommon::GetInstance();
+
+	modelTitle_ = Model::CreateFromOBJ("Title", true);
+
+	titleWorldTransform_.Initialize();
+	titleWorldTransform_.translation_.z -= 42.f;
+
+	viewProjection_.Initialize();
+
+	timer_ = 0.0f;
+}
 
 void TitleScene::Update() { 
 	if (Input::GetInstance()->PushKey(DIK_SPACE)) {
 		finished_ = true;
 	}
+
+	// タイマーを加算
+	timer_ += 1.0f / 60.0f;
+	// 上下アニメーション
+	float param = std::sin((2 * std::numbers::pi_v<float>)*timer_ / TitleTime);
+	float radian = TitlePositionStart + TitlePositionEnd * (param + 1.0f) / 2.0f;
+	titleWorldTransform_.translation_.y = sinf((radian * float(M_PI)) / 2);
+
+	titleWorldTransform_.UpdateMatrix();
 }
 
-void TitleScene::Draw() {}
+void TitleScene::Draw() {
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+	Model::PreDraw(commandList);
+
+	modelTitle_->Draw(titleWorldTransform_, viewProjection_);
+
+	Model::PostDraw();
+
+}
--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -1,4 +1,10 @@
+#include "Audio.h"
 #include "Input.h"
+#include "Model.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "DirectXCommon.h"
 
 class TitleScene {
 public:
@@ -15,5 +21,24 @@ private:
 	//フラグ
 	bool finished_ = false;
 
+	DirectXCommon* dxCommon_ = nullptr;
+
+	Model* modelTitle_ = nullptr;
+
+	WorldTransform titleWorldTransform_;
+
+	ViewProjection viewProjection_;
+
+	// 最初の角度
+	static inline const float TitlePositionStart = 0.1f;
+
+	// 最後の角度
+	static inline const float TitlePositionEnd = -0.1f;
+
+	// アニメーションの周期となる時間
+	static inline const float TitleTime = 3.0f;
+
+	// 経過時間
+	float timer_ = 0.0f;
 
 };
